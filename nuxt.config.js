05660ad51075bb9b656436b91b4cdb26e0a5aa9f
jamesninnes <james.ninnes@gmail.com>
2020-10-03 22:35:10 +1000

    Dockerfile

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,12 @@ export default {
     ]
   },
 
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false
+  },
+
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     'vuesax/dist/vuesax.css'
